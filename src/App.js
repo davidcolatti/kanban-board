f1e2fb0899ed895e3d9d80d23c23e8fa1c4b4a02
David Colatti <davidcolatti@gmail.com>
2020-07-02 14:48:12 -0400

    feat: imports and app set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
+
+const itemsFromBackend = [
+  { id: uuidv4(), content: "First task" },
+  { id: uuidv4(), content: "Second task" },
+];
+
+const columnsFromBackend = [
+  {
+    [uuidv4()]: {
+      name: "Todo",
+      items: itemsFromBackend,
+    },
+  },
+];
 
 function App() {
-  return <div className="App">App</div>;
+  const [columns, setColumns] = useState(columnsFromBackend);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      <DragDropContext onDropEnd={(result) => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "lightgrey",
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  ></div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
